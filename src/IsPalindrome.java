ebe5500a3b1fad07d2b4ba3ae88a5fc02638189a
senthuran <senthuran.manoharan@gmail.com>
2021-09-04 19:44:46 +0530

    Add IsPalindrome implementation

--- src/IsPalindrome.java
+++ src/IsPalindrome.java
@@ -1,2 +1,20 @@
 public class IsPalindrome {
+
+    public boolean checkPalindrom(String word) {
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) == word.charAt(word.length() - i - 1)) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        IsPalindrome isPalindrome = new IsPalindrome();
+        String testWord = "blaalb";
+        System.out.println(testWord + " is " + (isPalindrome.checkPalindrom(testWord) ? " a palindrome" : " not a palindrome"));
+
+    }
 }
